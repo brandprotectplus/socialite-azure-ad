f1bd00612e2f53401e92cf3bdb432e19583bfdb3
Joe Campo <jcampo@gmail.com>
2023-11-11 07:55:07 -0500

    Clean up

--- src/AzureOauthProvider.php
+++ src/AzureOauthProvider.php
@@ -11,7 +11,9 @@ use Laravel\Socialite\Two\InvalidStateException;
 class AzureOauthProvider extends AbstractProvider implements ProviderInterface
 {
     const IDENTIFIER = 'AZURE_OAUTH';
+
     protected $scopes = ['User.Read'];
+
     protected $scopeSeparator = ' ';
 
     protected function getAuthUrl($state)
@@ -72,7 +74,6 @@ class AzureOauthProvider extends AbstractProvider implements ProviderInterface
             'id'                => $user['id'],
             'name'              => $user['displayName'],
             'email'             => $user['mail'],
-
             'businessPhones'    => $user['businessPhones'],
             'displayName'       => $user['displayName'],
             'givenName'         => $user['givenName'],
